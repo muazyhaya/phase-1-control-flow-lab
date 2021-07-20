84e2db778cd17554ec50c9848cf61813646d4959
Muaz Yhaya <muazyhaya@outlook.com>
2021-07-19 17:25:00 -0700

    commit tree ecc01827b83badacd1a37b29e5c0ed68d6e60100

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return "This one is on me!"}
+    if (distance > 2500) {
+      return "No can do."}
+    if (distance > 2000) {
+      return "I will gladly take your thirty bucks."}
+    }
+
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  if (city !== "NYC") {
+    return "No go."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+      case 'not as generous':
+        return 'Thank you.';
+        break; 
+        default:
+          return 'Bye.';
+          break;
+  }
 }
